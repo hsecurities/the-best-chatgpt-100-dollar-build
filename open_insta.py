78223412eb630acc80d77f5391519e988310bc4a
hsecurities <118465547+hsecurities@users.noreply.github.com>
2025-10-16 19:05:38 +0530

    Create open_insta.py

---
+++ open_insta.py
@@ -0,0 +1,9 @@
+import webbrowser
+
+def open_insta():
+    url = "https://www.instagram.com/tech.exprt/"
+    print("🔗 Opening Instagram profile: @tech.exprt ...")
+    webbrowser.open(url)
+
+if __name__ == "__main__":
+    open_insta()
